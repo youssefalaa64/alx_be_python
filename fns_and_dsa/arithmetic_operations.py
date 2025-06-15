82a00f45e608e4a7063a544d13aa2752a71fa6dd
Youssef alaa <youssefaeldin@gmail.com>
2025-06-15 21:42:29 +0300

    done

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,7 +6,7 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply" :
         return num1 * num2
     elif operation == "divide" :
-        if num2 == 0 :
+        if num2 == 0:
             print('you cant divide by zero')
         else :
             return num1 / num2
