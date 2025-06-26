e8c774a8851460526f6a91e236bda22a48bd2ef7
Youssef alaa <youssefaeldin@gmail.com>
2025-06-26 12:45:16 +0300

    trying

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,14 +2,12 @@ class BankAccount:
     def __init__(self, initial_balance=0):
         account_balance = initial_balance
     def deposit(self, amount) :                     
-        self.account_balance += amount
+        self.initial_balance += amount
     def withdraw(self, amount) :
-        if amount >= self.account_balance :
-            self.account_balance -= amount
+        if amount >= self.initial_balance :
+            self.initial_balance-= amount
             return True
         else :
             return False
     def display_balance(self) :
-        print(f"Current Balance: ${self.account_balance: }")
-    
-        
+        print(f"Current Balance: ${self.initial_balance: }")
