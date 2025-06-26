2d5078a967f97ae0bd67b185f61108f57c32911a
Youssef alaa <youssefaeldin@gmail.com>
2025-06-26 17:46:36 +0300

    ioef

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,11 +4,11 @@ class test_calculator(unittest.TestCase) :
     def setUp(self):
         self.calc = SimpleCalculator()
     def test_addition(self) :
-        self.assertEqual(self.calc(5, 5), 10)
+        self.assertEqual(self.calc.add(5, 5), 10)
     def test_subtraction(self) :
-        self.assertEqual(self.calc(5, 2 ), 3)
+        self.assertEqual(self.calc.subtract(5, 2 ), 3)
     def test_multiplication(self):
-        self.assertEqual(self.calc(5, 5), 25)
+        self.assertEqual(self.calc.multiply(5, 5), 25)
     def test_divide(self):
-        self.assertEqual(self.calc(10,5), 2)
+        self.assertEqual(self.calc.divide(10,5), 2)
 
