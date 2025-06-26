ecf4871d0d540f94a941cffb0d4f205342278c6c
Youssef alaa <youssefaeldin@gmail.com>
2025-06-26 12:15:51 +0300

    done

--- bank_account.py
+++ bank_account.py
@@ -10,6 +10,6 @@ class BankAccount :
         else :
             return False
     def display_balance(self) :
-        print(f"Current Balance: ${self.account_balance:.2f}")
+        print(f"Current Balance: ${self.account_balance: }")
     
         
