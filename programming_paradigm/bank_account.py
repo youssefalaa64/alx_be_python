641876b2fa0e051890cec53d20c8283c3c74aab2
Youssef alaa <youssefaeldin@gmail.com>
2025-06-26 13:00:39 +0300

    im done

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -10,4 +10,4 @@ class BankAccount:
         else :
             return False
     def display_balance(self) :
-        print(f"Current Balance: ${self.account_balance: }")
+        print(f"Current Balance: ${float(self.account_balance): }")
