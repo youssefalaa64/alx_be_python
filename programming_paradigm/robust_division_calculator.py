f4302cde52cc92c870f6ecdf5939d0d6c2ab328e
Youssef alaa <youssefaeldin@gmail.com>
2025-06-26 13:04:49 +0300

    baby

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,7 +4,7 @@ def safe_divide(numerator, denominator) :
         float_den = float(denominator)
         return float_num / float_den
     except ZeroDivisionError :
-        return "you cannot devide by zero"
+        return "Error: Cannot divide by zero."
     except ValueError :
         return "sorry, invalid input"
     except Exception as e :
