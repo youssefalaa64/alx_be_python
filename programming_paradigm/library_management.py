0792a2f02d85f13726e29aa2109617b935681985
Youssef alaa <youssefaeldin@gmail.com>
2025-06-26 15:31:21 +0300

    efjh

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,9 +1,9 @@
-class book :
+class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
         self._is_checked_out = False
-class library :
+class Library:
     def __init__(self):
         self._books = []
     def add_book(self, book) :
