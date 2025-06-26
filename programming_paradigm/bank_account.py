e779949d5c0c991f9cd4b3748608a3a114c49835
Youssef alaa <youssefaeldin@gmail.com>
2025-06-26 12:55:02 +0300

    dd

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,6 @@
 class BankAccount:
     def __init__(self, initial_balance=0):
-        account_balance = initial_balance
+        self.account_balance = initial_balance
     def deposit(self, amount) :                     
         self.account_balance += amount
     def withdraw(self, amount) :
