02b574c4025d033ab2b512543809f3c5bb652c23
Youssef alaa <youssefaeldin@gmail.com>
2025-07-04 17:47:30 +0300

    weoi

---
+++ oop/library_system.py
@@ -0,0 +1,23 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+class EBook(Book):
+    def __init__(self, title, author, file_size:int):
+        super().__init__(title, author)
+        self.file_size = file_size
+class PrintBook(Book):
+    def __init__(self, title, author, page_count:int):
+        super().__init__(title, author)
+        self.page_count = page_count
+class Library:
+    def __init__(self):
+        self.books = []
+    def add_book(self, book):
+        self.books.append(book)
+    def list_books(self):
+        for Book in self.books:
+            print(Book)
+    
+         
+        
