c71ce804fc334b693e854414dd4d89429b7788aa
Youssef alaa <youssefaeldin@gmail.com>
2025-07-04 18:42:48 +0300

    jde

--- oop/library_system.py
+++ oop/library_system.py
@@ -15,7 +15,7 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
     def __str__(self):
-        return f"PrintBook: {self.title} BY {self.author}, Page Count: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 class Library:
     def __init__(self):
         self.books = []
